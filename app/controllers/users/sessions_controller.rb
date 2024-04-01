e9628221d7178c1a428b7add4ff945306049bde0
eric mutembei <pychangeri@gmail.com>
2024-04-02 00:35:09 +0300

    account done setting

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -19,7 +19,8 @@ class Users::SessionsController < Devise::SessionsController
 
   # DELETE /resource/sign_out
   def destroy
-    super
+    sign_out current_user
+    redirect_to root_path, notice: 'Logged out successfully.'
   end
 
   # protected
